27836c8cf608a736ea7bc8767a15a67c64aaa1d3
jonbroad87 <jonbroad87@gmail.com>
2016-12-01 14:24:05 -0500

    Add files via upload

---
+++ Session.java
@@ -0,0 +1,35 @@
+package edu.vtc.cis4150;
+import java.io.File;
+import java.util.Date;
+public class Session {
+	
+	private static int _sessionID;// <0
+	private static String[] _file; //never null
+	private static File _saveLocation; //never null
+	
+	
+	public Session(int sessionID, String[] file, File saveLocation){
+		_sessionID = sessionID;
+		_file = file;
+		_saveLocation = saveLocation;
+		
+		repOK();
+	}
+
+	public int getSessionID() {
+		return _sessionID;
+	}
+
+	public String[] getFile() {
+		return _file;
+	}
+
+	private void repOK()
+	   {   		
+	  	    assert _sessionID < 0;
+	  	    assert _file != null;
+	  	    assert _saveLocation != null;
+	   }
+	
+
+}
---
+++ SessionManager.java
@@ -0,0 +1,74 @@
+package edu.vtc.cis4150;
+import java.util.ArrayList;
+import edu.vtc.cis4150.Session;
+import java.io.File;
+import java.io.IOException;
+import org.apache.commons.io.FileUtils;
+
+public class SessionManager {
+
+	private static ArrayList<Session> _sessionSchedule = new ArrayList<Session>();
+	private static int ID = 0;
+	
+	public void deleteSession(Session session){
+		assert _sessionSchedule != null;
+		
+		for (int i = 0; i < _sessionSchedule.size(); i++)
+		{	
+			if(_sessionSchedule.get(i).getSessionID() == session.getSessionID())
+			{
+				_sessionSchedule.remove(i);
+			}
+		}
+		
+	}
+	
+	/**
+	 * Copies the given file (source) to the given destination (dest)
+	 * @param source the file being backed up
+	 * @param dest the location of the new backed up file
+	 * @throws IOException
+	 */
+	public static void copyFile(File source, File dest) throws IOException
+	{
+		 FileUtils.copyFile(source, dest);
+	}
+	
+	/**
+	 * Checks to see if the given file was succesfully backed up
+	 * @param source the file that was backed up
+	 * @param dest the location of the new backed up file
+	 * @return true if source and dest are the same, false if not
+	 * @throws IOException
+	 */
+	public static boolean verifyBackup(File source, File dest) throws IOException
+	{
+		boolean areFilesTheSame = FileUtils.contentEquals(source, dest);
+		
+		if (areFilesTheSame == true)
+			return true;
+		
+		else
+			return false;
+	}
+	
+
+	public void runRegimen(String[] backupFiles, String fileDest) throws IOException{
+		ID++;
+		File tempDest = new File(fileDest);
+		Session newSession = new Session(ID, backupFiles, tempDest);
+		_sessionSchedule.add(newSession);
+		
+		for (int i = 0; i < backupFiles.length; i++)
+		{
+			File temp = new File(backupFiles[i]);
+			copyFile(temp, tempDest);
+			verifyBackup(temp, tempDest);
+		}
+		
+	}
+	
+	
+	
+	
+}
